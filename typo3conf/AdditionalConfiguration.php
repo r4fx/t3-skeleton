a807cb95bc4064682482c3b1d33f2391bfc21b20
Rafał Brzeski <rafal@edgetechnology.pl>
2016-01-10 17:25:03 +0100

    added AdditionalConfiguration.php file

---
+++ typo3conf/AdditionalConfiguration.php
@@ -0,0 +1,8 @@
+<?php
+
+# Backend Settings
+$GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] = 'files/';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['RTE_imageStorageDir'] = 'files/public/uploads/';
+
+# Frontend Settings
+$GLOBALS['TYPO3_CONF_VARS']['FE']['additionalAbsRefPrefixDirectories'] = 'files/';
