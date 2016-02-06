6e63674449d7690214bbbe7f0b3ec4bc9488407f
Rafał Brzeski <rafal@edgetechnology.pl>
2016-02-06 02:30:50 +0100

    remove global user config //bug

--- typo3conf/AdditionalConfiguration.php
+++ typo3conf/AdditionalConfiguration.php
@@ -5,7 +5,7 @@ $GLOBALS['TYPO3_CONF_VARS']['BE']['fileadminDir'] = 'files/';
 $GLOBALS['TYPO3_CONF_VARS']['BE']['RTE_imageStorageDir'] = 'files/public/uploads/';
 
 # Default BE User settings
-$GLOBALS['TYPO3_CONF_VARS']['BE']['defaultUserTSconfig'] = '<INCLUDE_TYPOSCRIPT: source="DIR:files/assets/typoscript/tsconfig_user/" extensions="tsu">';
+//$GLOBALS['TYPO3_CONF_VARS']['BE']['defaultUserTSconfig'] = '<INCLUDE_TYPOSCRIPT: source="FILE:files/assets/typoscript/tsconfig_user/tsconfig.tsu">';
 
 # Frontend Settings
 $GLOBALS['TYPO3_CONF_VARS']['FE']['additionalAbsRefPrefixDirectories'] = 'files/';
